e7bbb1ea8fcc5fda59dd9493b0476ee7f68560dd
Brian Pope <pope.brian@gmail.com>
2020-09-30 10:23:37 -0500

    edited text

--- src/components/about.js
+++ src/components/about.js
@@ -4,8 +4,8 @@ import aboutStyles from '../pages/about.module.scss'
 const About = () => {
     return (
         <>
-            <p className={aboutStyles.opi}>I am a Senior Design Consultant at <a href="https://objectpartners.com/">Object Partners</a> in Omaha. With over 20 years of design experience I have seen a variety of different projects, processes and teams. I am passionate about understanding the "Why" behind design, developing apps as a team and the education of design.</p>
-            <p className={aboutStyles.connectaha}>I am a regional technology conference speaker and co-founder of <a href="https//connectah.com/">Connectaha Technology Conference</a></p>
+            <p className={aboutStyles.connectaha}>I am a Senior Design Consultant at <a href="https://objectpartners.com/">Object Partners</a>/ <a href="https://improving.com/">Improving</a> in Omaha. With over 20 years of design experience I have seen a variety of different projects, processes and teams. I am passionate about understanding the "Why" behind design, developing apps as a team and the education of design.</p>
+            <p className={aboutStyles.connectaha}>I am a regional technology conference speaker and co-founder of <a href="https//connectah.com/">Connectaha Technology Conference.</a></p>
         </ >
     )
 }
