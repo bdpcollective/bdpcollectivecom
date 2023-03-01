0e0762d1adeaa3357c78c2bd683777dc7ba902f9
Brian Pope <32911149+bdpcollective@users.noreply.github.com>
2023-03-01 10:21:53 -0600

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import aboutStyles from '../pages/about.module.scss'
 const About = () => {
     return (
         <>
-            <p className={aboutStyles.connectaha}>I am Senior Web Designer at <a href="https://www.trility.io/">Trility Consulting</a> in Omaha. With over 20 years of design experience I have seen a variety of different projects, processes and teams. I am passionate about understanding the "Why" behind design, developing apps as a team and the education of design.</p>
+            <p className={aboutStyles.connectaha}>I am Principal UX Designer at <a href="https://www.devobsessed.com/">DevObsessed</a> in Omaha. With over 20 years of design experience I have seen a variety of different projects, processes and teams. I am passionate about understanding the "Why" behind design, developing apps as a team and the education of design.</p>
             <p className={aboutStyles.connectaha}>I am a regional technology conference speaker and co-founder of <a href="https//connectah.com/">Connectaha Technology Conference.</a></p>
         </ >
     )
