0cee8d01a0462fedb015735b8e60db85f311b0a7
Brian Pope <brian.pope@devobsessed.com>
2025-03-23 19:52:13 -0500

    Add GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://bdpcollective.github.io',
+  base: '/website',
   integrations: [tailwind()],
 });
