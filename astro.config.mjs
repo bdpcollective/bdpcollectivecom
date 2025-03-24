ea166639d255e9099acb198fdc7ec6042f8c6ff9
Brian Pope <brian.pope@devobsessed.com>
2025-03-23 20:29:59 -0500

    fix: update base URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.bdpcollective.com',
-  base: '/',
+  base: '/bdpcollectivecom',
   integrations: [tailwind()],
 });
