f18410e99b79cb8389d82bb9a081c30c5d5f4e23
Brian Pope <brian.pope@devobsessed.com>
2025-03-23 22:04:49 -0500

    Fix navigation links by adding forward slashes after base URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bdpcollective.github.io/bdpcollectivecom',
-  base: '/bdpcollectivecom',
+  base: '/bdpcollectivecom/',
   integrations: [tailwind()],
 });
