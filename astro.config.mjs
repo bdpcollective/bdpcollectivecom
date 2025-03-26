a30567303828f1ebeea86af92a2f9024369cc233
Brian Pope <brian.pope@devobsessed.com>
2025-03-25 22:03:05 -0500

    Fix site URL in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.bdpcollective.com',
+  site: 'https://bdpcollective.github.io',
   base: '/',
   integrations: [tailwind()],
 });
